814b2737f147bd3691519f0c07ca95d9eb1bd499
Gleidson Medrado <gleidsonmedrado@hotmail.com>
2023-10-30 17:19:06 -0300

    Adicionando conteúdo

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View, ScrollView, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, StatusBar, Image } from 'react-native';
 import React from "react" 
 
 export default function App() {
   return <ScrollView>
     <StatusBar barStyle="light-content" backgroundColor="#19282F"/>
-    
-    <Text> Oi, meu primeiro App! </Text>
+
+    <View>
+      <Text> Titulo do App </Text>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+      </Text>
+
+      <Image source={{ uri: "https://picsum.photos/200/200" }}/>
+    </View>
   </ScrollView>;
 }
 
