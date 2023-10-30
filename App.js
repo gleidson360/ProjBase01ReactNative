3b1c34ff2de62dcb05bd6fc81edd3aecd7b60b63
Gleidson Medrado <gleidsonmedrado@hotmail.com>
2023-10-30 17:15:12 -0300

    Incluindo ScrollView e StatusBar

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, StatusBar } from 'react-native';
 import React from "react" 
 
 export default function App() {
-  return <View>
+  return <ScrollView>
+    <StatusBar barStyle="light-content" backgroundColor="#19282F"/>
+    
     <Text> Oi, meu primeiro App! </Text>
-  </View>;
+  </ScrollView>;
 }
 
 const styles = StyleSheet.create({
