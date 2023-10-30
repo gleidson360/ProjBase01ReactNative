16903ebac881bdc0ab47fa6753ede0a71914aaad
Gleidson Medrado <gleidsonmedrado@hotmail.com>
2023-10-30 17:11:39 -0300

    Iniciando criação

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from "react" 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <View>
+    <Text> Oi, meu primeiro App! </Text>
+  </View>;
 }
 
 const styles = StyleSheet.create({
